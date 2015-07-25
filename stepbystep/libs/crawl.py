8a644bef83bcafea3027713a3621102c9a81509b
nplus <junnplus@qq.com>
2015-07-25 12:35:24 +0800

    ignore

--- stepbystep/libs/crawl.py
+++ stepbystep/libs/crawl.py
@@ -4,8 +4,6 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from scrapy.utils.log import configure_logging
 
-from base64 import b64encode
-
 settings = get_project_settings()
 
 class AccountCrawler():
