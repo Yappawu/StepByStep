2e72604c9a47014cad56bc7104fe50248512bf91
nplus <junnplus@qq.com>
2015-07-25 13:09:03 +0800

    init form

---
+++ stepbystep/forms/auth/login.py
@@ -0,0 +1,19 @@
+from flask_wtf import Form
+from wtforms import PasswordField, StringField, BooleanField
+from wtforms.validators import Email, Required, ValidationError
+
+from stepbystep.models import UserModel
+
+class LoginForm(Form):
+    username = StringField('Username', [Required()])
+    password = PasswordField('Password', [Required()])
+    remember_me = BooleanField('Remember Me')
+
+    def validate_password(self, field):
+        user = UserModel.objects.filter(
+            username=self.username.data
+        ).first()
+        if user is not None and user.verify_password(field.data):
+            self.user = user
+        else:
+            raise ValidationError(u'Username or password is invalid')
