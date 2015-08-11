c75d5b8036b32401d260aa98f95020005fbe0167
nplus <junnplus@qq.com>
2015-08-11 16:44:06 +0800

    crawler

--- stepbystep/libs/crawl.py
+++ stepbystep/libs/crawl.py
@@ -2,16 +2,17 @@ from billiard import Process
 
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from scrapy.utils.log import configure_logging
+from scrapy.utils.log import configure_logging  # noqa
 
 settings = get_project_settings()
+
 
 class AccountCrawler():
 
     def __init__(self):
         self.crawler = CrawlerProcess(settings)
 
-    def _crawl(self, origin_oj, username, password):
+    def _crawl(self, origin_oj, username):
         self.crawler.crawl(
             origin_oj + '_user',
             username=username,
@@ -19,13 +20,12 @@ class AccountCrawler():
         self.crawler.start()
         self.crawler.stop()
 
-    def crawl(self, origin_oj, username, password):
+    def crawl(self, origin_oj, username):
         p = Process(
             target=self._crawl,
             args=[
                 origin_oj,
                 username,
-                password
             ]
         )
         p.start()
--- stepbystep/libs/tasks.py
+++ stepbystep/libs/tasks.py
@@ -5,12 +5,15 @@ from crawl import (
     AccountCrawler
 )
 
+
 def make_celery(app):
     celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
     TaskBase = celery.Task
+
     class ContextTask(TaskBase):
         abstract = True
+
         def __call__(self, *args, **kwargs):
             with app.app_context():
                 return TaskBase.__call__(self, *args, **kwargs)
@@ -19,6 +22,7 @@ def make_celery(app):
 
 celery = make_celery(app)
 
+
 @celery.task()
 def account_crawler(origin_oj, username):
     crawler = AccountCrawler()
@@ -26,3 +30,17 @@ def account_crawler(origin_oj, username):
         origin_oj,
         username,
     )
+
+
+@celery.task()
+def sdut_schedule():
+    from stepbystep.models import AccountItem
+    for account in AccountItem.objects(origin_oj='sdut').all():
+        account_crawler(account.origin_oj, account.username)
+
+
+@celery.task()
+def poj_schedule():
+    from stepbystep.models import AccountItem
+    for account in AccountItem.objects(origin_oj='poj').all():
+        account_crawler(account.origin_oj, account.username)
