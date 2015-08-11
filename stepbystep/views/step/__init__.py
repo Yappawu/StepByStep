7afec298df743bb56b85c9708de118a859a78c89
nplus <junnplus@qq.com>
2015-08-11 16:46:23 +0800

    url

--- stepbystep/views/step/__init__.py
+++ stepbystep/views/step/__init__.py
@@ -1,12 +1,20 @@
 from flask import Blueprint
 from .poj import StepPojView
+from .sdut import StepSdutView
 
 
 bp_step = Blueprint('step', __name__)
 
 bp_step.add_url_rule(
-    '/poj',
+    '/poj/<ordinal>',
     endpoint='poj',
     view_func=StepPojView.as_view('poj'),
+    methods=['get', 'post']
+)
+
+bp_step.add_url_rule(
+    '/sdut/<ordinal>',
+    endpoint='sdut',
+    view_func=StepSdutView.as_view('sdut'),
     methods=['get', 'post']
 )
